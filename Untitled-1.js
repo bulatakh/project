db0ed631002ce44b60cd0ced47b87a57e30b5f13
bulat.a@mail.ru <bulat.a@mail.ru>
2019-09-03 15:09:48 +0300

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,20 @@
+//'use strict';
+
+a = 5;
+console.log(a);
+
+let incr = 10,
+    decr = 10;
+
+   
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log("2"===2);
+
+    let isChecked = false,
+        isClose = false;
+
+        console.log(isChecked || !isClose);
